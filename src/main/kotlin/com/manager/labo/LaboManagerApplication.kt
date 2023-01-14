9374d12a17f1467ada86ddcd56eb3172874ece69
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2023-01-14 12:08:54 +0100

    core files

---
+++ src/main/kotlin/com/manager/labo/LaboManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.manager.labo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaboManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LaboManagerApplication>(*args)
+}
