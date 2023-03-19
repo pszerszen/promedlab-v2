10dac544c52c4d4a58886e1231cbf1360a25639a
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2023-03-19 15:46:24 +0100

    adding missing validations

--- src/main/kotlin/com/manager/labo/model/ExaminationSummaryModel.kt
+++ src/main/kotlin/com/manager/labo/model/ExaminationSummaryModel.kt
@@ -2,28 +2,35 @@ package com.manager.labo.model
 
 import com.manager.labo.utils.DisplayInJTable
 import com.manager.labo.view.components.TableModel
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
 
 class ExaminationSummaryModel {
     var id: Long? = null
 
+    @Pattern(regexp = "[A-Z]\\d{2}")
     @DisplayInJTable(name = TableModel.EXAMINATIONS_SET, order = 0)
     var code: String? = null
 
     @DisplayInJTable(name = TableModel.EXAMINATIONS_SET, order = 1)
     var description: String? = null
 
+    @NotNull(message = "Nie wpisano wykonującego badania.")
     @DisplayInJTable(name = TableModel.EXAMINATIONS_SET, order = 2)
     var staffName: String? = null
 
+    @NotNull(message = "Należy uzupełnić wartość badania.")
     @DisplayInJTable(name = TableModel.EXAMINATIONS_SET, order = 3)
     var value: Int? = null
 
-    constructor(){}
+    constructor()
 
-    constructor(code: String?, description: String?) {
-        this.code = code
-        this.description = description
-    }
+    constructor(code: String?, description: String?) : this(
+            id = null,
+            code = code,
+            description = description,
+            staffName = null,
+            value = null)
 
     constructor(id: Long?, code: String?, description: String?, staffName: String?, value: Int?) {
         this.id = id
