f9f32fbcedc050d4654863c64457e4bf9746696e
Piotr Szerszeń <piotr.b.szerszen@icloud.com>
2023-04-14 10:19:41 +0200

    adding couple unit tests in ExaminationServiceTest

--- src/test/kotlin/com/manager/labo/service/ExaminationServiceTest.kt
+++ src/test/kotlin/com/manager/labo/service/ExaminationServiceTest.kt
@@ -1,18 +1,21 @@
 package com.manager.labo.service
 
 import com.manager.labo.entities.Examination
+import com.manager.labo.entities.ExaminationDetails
 import com.manager.labo.mapper.ExaminationDetailsMapper
 import com.manager.labo.mapper.ExaminationMapper
+import com.manager.labo.model.ExaminationModel
 import com.manager.labo.model.ExaminationRequestModel
 import com.manager.labo.repository.ExaminationDetailsRepository
 import com.manager.labo.repository.ExaminationRepository
-import com.manager.labo.repository.PatientRepository
-import io.mockk.MockKAnnotations
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.slf4j.Logger
@@ -22,14 +25,14 @@ import java.util.*
 @ExtendWith(MockKExtension::class)
 class ExaminationServiceTest {
 
-    private var model: ExaminationRequestModel = ExaminationRequestModel()
+    private val EXAMINATION_ID = 1L
+
+    private var model = ExaminationRequestModel()
 
     @MockK
     lateinit var examinationRepository: ExaminationRepository
     @MockK
     lateinit var examinationDetailsRepository: ExaminationDetailsRepository
-    @MockK
-    lateinit var patientRepository: PatientRepository
     @MockK
     lateinit var examinationDetailsMapper: ExaminationDetailsMapper
     @MockK
@@ -42,14 +45,26 @@ class ExaminationServiceTest {
     @InjectMockKs
     lateinit var tested: ExaminationService
 
-    @BeforeEach
-    fun setUp() {
-        MockKAnnotations.init(this)
+
+    @Test
+    fun all(@MockK examination: Examination, @MockK examinationModel: ExaminationModel) {
+        every { examinationRepository.findAll() } returns listOf(examination)
+        every { examinationMapper.toExaminationModel(examination) } returns examinationModel
+
+        tested.all.let {
+            it shouldHaveSize 1
+            it shouldContain examinationModel
+        }
     }
 
     @Test
-    fun all(@MockK examination: Examination) {
+    fun `should map to ExaminationRequestModel`(@MockK details: ExaminationDetails,
+                                                @MockK examination: Examination) {
+        every { examination.examinationDetails } returns mutableSetOf(details)
+        every { examinationRepository.getReferenceById(EXAMINATION_ID) } returns examination
+        every { examinationMapper.toExaminationRequestModel(eq(examination), any())  } returns model
 
+        tested.getExaminationRequestModel(EXAMINATION_ID) shouldBe model
     }
 
     companion object {
